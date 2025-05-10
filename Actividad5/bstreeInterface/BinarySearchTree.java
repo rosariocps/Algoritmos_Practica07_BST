2ce813dcaa02a36ee3b449c0d187c0705f30b031
KahoriDiaz <kahori.diaz@estudiante.ucsm.edu.pe>
2025-05-10 01:20:26 -0500

    Create BinarySearchTree.java

---
+++ Actividad5/bstreeInterface/BinarySearchTree.java
@@ -0,0 +1,6 @@
+public interface BinarySearchTree<E> {
+  void insert (E data) throws ItemDuplicated;
+  E search (E data) throws ItemNotFound;
+  void delete (E data) throws ExceptionIsEmpty;
+  boolean isEmpty();
+}
