5924e11a0488f3e60a82af25d1ae9025eb361f7e
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-10 14:37:40 -0500

    Desarrollo de clase ItemNoFound

---
+++ exceptions/ItemNoFound.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ItemNoFound extends Exception {
+    public ItemNoFound(String msg) {
+        super(msg);
+    }
+
+    public ItemNoFound() {
+        super();
+    }
+}
