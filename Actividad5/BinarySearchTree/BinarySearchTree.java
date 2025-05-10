df2857a40a7fbafe4329afb6dd4615190f60e686
KahoriDiaz <kahori.diaz@estudiante.ucsm.edu.pe>
2025-05-10 01:22:11 -0500

    Update BinarySearchTree.java

--- Actividad5/BinarySearchTree/BinarySearchTree.java
+++ Actividad5/BinarySearchTree/BinarySearchTree.java
@@ -1,1 +1,6 @@
-a
+public interface BinarySearchTree<E> {
+  void insert (E data) throws ItemDuplicated;
+  E search (E data) throws ItemNotFound;
+  void delete (E data) throws ExceptionIsEmpty;
+  boolean isEmpty();
+}
